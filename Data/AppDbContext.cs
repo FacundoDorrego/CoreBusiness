f276889a165dd42e1ce126349562e982b8165ad6
Facundo <124022262+FacundoDorrego@users.noreply.github.com>
2025-02-23 05:57:48 -0300

    chau bootstrap

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace CoreBusiness.Data
+{
+
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Producto> Productos { get; set; } // Agrega más tablas aquí
+    }
+
+}
--- Models/Producto.cs
+++ Models/Producto.cs
@@ -1,13 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Clases
+
+namespace CoreBusiness
 {
     public class Producto
     {
+        [Key]  // Indica que es la clave primaria
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]  // Marca como autoincremental
         public int ProductoID { get; set; }
         public string Descripcion { get; set; }
         public string Nombre { get; set; }
